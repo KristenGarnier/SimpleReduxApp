fb204e629e1e44904cd472b467795a03a7e4f900
KristenGarnier <garnier.kristen@icloud.com>
2016-06-30 15:11:44 +0200

    Feature() : Add redux to the course page component

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,13 +1,64 @@
 import React, {PropTypes} from 'react'
+import {connect} from 'react-redux'
+import * as courseActions from '../../actions/courseActions'
 
 class CoursesPage extends React.Component {
+  constructor (props, context) {
+    super(props, context)
+
+    this.state = {
+      course: { title: '' }
+    }
+
+    this.onTitleChange = this.onTitleChange.bind(this)
+    this.onClickSave = this.onClickSave.bind(this)
+  }
+
+  onTitleChange (event) {
+    this.setState({course: Object.assign(
+      this.state.course,
+      {
+        title: event.target.value
+      }
+    )})
+  }
+
+  onClickSave () {
+    this.props.dispatch(courseActions.createCourse(this.state.course))
+  }
+
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>
+  }
+
   render () {
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}/>
+
+        <input
+          type="submit"
+          value="save"
+          onClick={this.onClickSave}/>
       </div>
     )
   }
 }
 
-export default CoursesPage
+CoursesPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+}
+
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  }
+}
+
+export default connect(mapStateToProps)(CoursesPage)
